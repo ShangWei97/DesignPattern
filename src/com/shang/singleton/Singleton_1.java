ec9fb5b0606e5c50c3b2c2df9927b4d3bfb99140
shangwei <shangwei@tuhu.n>
2021-06-09 20:11:12 +0800

    添加懒汉线程不安全

--- src/com/shang/singleton/Singleton_1.java
+++ src/com/shang/singleton/Singleton_1.java
@@ -6,6 +6,16 @@ package com.shang.singleton;
  * @author shangwei
  */
 public class Singleton_1 {
+    private static Singleton_1 singleton_1;
 
+    private Singleton_1() {
+    }
+
+    public static Singleton_1 getInstance() {
+        if (singleton_1 == null) {
+            singleton_1 = new Singleton_1();
+        }
+        return singleton_1;
+    }
 
 }
